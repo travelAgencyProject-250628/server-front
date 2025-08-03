8ff9aa2e0a2f706194ac0e87ec12a58d9bbfd2fb
yyoungjin <emzmfdudwls@naver.com>
2025-08-03 18:01:07 +0900

    hotfix: 태그관리->서브카테고리 텍스트 수정

--- travel-agency/src/components/AdminLayout.vue
+++ travel-agency/src/components/AdminLayout.vue
@@ -34,7 +34,7 @@
           <svg width="20" height="20" viewBox="0 0 24 24" fill="none">
             <path stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"/>
           </svg>
-          <span>태그관리</span>
+          <span>서브카테고리</span>
         </router-link>
         
         <router-link to="/admin/products" class="nav-item">
